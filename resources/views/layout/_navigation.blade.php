02349247a2ea503bde09c46d558e5d1f17c93613
Yassin Makhloufi <yassin@studi.se>
2018-06-24 01:12:04 +0200

    build basic navbar

---
+++ resources/views/layout/_navigation.blade.php
@@ -0,0 +1,123 @@
+<style>
+    .search {
+        color: #ccc;
+        padding: 0 15px;
+        background-color: white;
+        border-radius: 20px;
+        height: 30px;
+        border: 1px solid #ddd;
+        min-width: 100px;
+    }
+
+    .search > i {
+        max-height: 30px;
+        min-height: 30px;
+        cursor: pointer;
+    }
+
+    .search > input {
+        margin: 0 5px;
+        border-style: none;
+        max-height: 26px;
+        display: inline-block;
+        top: -9px;
+        color: #444;
+    }
+
+    .search > input::placeholder {
+        color: #ccc;
+    }
+
+    .search > input:focus {
+        outline: none;
+    }
+
+    @media (max-width: 460px) {
+        .search {
+            width: 150px;
+        }
+
+        .search > input {
+            width: 66%;
+        }
+    }
+</style>
+<nav class="navbar navbar-dark bg-dark justify-content-between">
+    <a class="navbar-brand" href="/">
+        <img src="{{asset('/img/logo_white.png')}}" height="28">
+        <span class="d-none d-sm-inline"> Personals</span>
+    </a>
+    <form action="/search" method="get" id="search-form" class="search form-inline">
+        <i class="fa fa-search" onclick="
+                            if(_('search-input').value !== ''){
+                              _('search-form').submit();
+                            } else {
+                              _('search-input').focus();
+                            }"></i>
+
+        <input type="text"
+               class="navbar-item"
+               placeholder="{{__('search...')}}"
+               name="q"
+               id="search-input"
+               autocomplete="off"
+        />
+        <i class="fa fa-times" onclick="_('search-form').reset();"></i>
+    </form>
+    <a class="btn btn-success" href="/write">
+        <span class="icon"><i class="fa fa-pencil-alt"></i></span>
+        <span class="d-none d-sm-inline"> {{_('Write Ad')}}</span>
+    </a>
+</nav>
+
+
+<a class="navbar-item" href="https://bulma.io">
+
+</a>
+
+<div class="navbar-menu" id="navMenu">
+    <div class="navbar-item">
+
+
+    </div>
+</div>
+</div>
+</div>
+
+<div class="navbar-end">
+    <div class="navbar-item  is-right">
+    </div>
+</div>
+</nav>
+
+<script language="JavaScript">
+  function _ (id) {
+    return document.getElementById(id)
+  }
+
+  document.addEventListener('DOMContentLoaded', function () {
+
+    // Get all "navbar-burger" elements
+    let $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0)
+
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+
+      // Add a click event on each of them
+      $navbarBurgers.forEach(function ($el) {
+        $el.addEventListener('click', function () {
+
+          // Get the target from the "data-target" attribute
+          let target = $el.dataset.target
+          let $target = document.getElementById(target)
+
+          // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+          $el.classList.toggle('is-active')
+          $target.classList.toggle('is-active')
+
+        })
+      })
+    }
+
+  })
+</script>
---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script
+            src="https://code.jquery.com/jquery-3.3.1.min.js"
+            integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+            crossorigin="anonymous"></script>
+    <script src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"
+            integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T"
+            crossorigin="anonymous"></script>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB"
+          crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.bundle.min.js"
+            integrity="sha384-u/bQvRA/1bobcXlcEYpsEdFVK/vJs3+T+nXLsBYJthmdBuavHvAW6UsmqO2Gd/F9"
+            crossorigin="anonymous"></script>
+    <title>@yield('title')</title>
+
+    <!-- Styles -->
+    <style>
+        html, body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Tahoma', sans-serif;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .full-height {
+            height: 100vh;
+        }
+
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+
+        .position-ref {
+            position: relative;
+        }
+
+        .content {
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+@include('layout._navigation')
+
+<div class="flex-center position-ref full-height">
+    <div class="content">
+        @yield('content')
+    </div>
+</div>
+</body>
+</html>
